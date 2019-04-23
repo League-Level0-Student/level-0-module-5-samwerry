8ed8981f520251936008146757c6ad9b94d3d5a4
League <league@iMac-6.attlocal.net>
2019-04-23 16:56:08 -0700

    finished two things in ellipse

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,26 +11,35 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name= JOptionPane.showInputDialog("Whats your name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+for(int i =0; i <name.length(); i++) {
+		 String bob = name.substring(i, i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
+		 if(i%2==0) {
+			 bob.toUpperCase();
+			 goofyName=goofyName+bob.toUpperCase();
+		 }
+		 else {
+			 bob.toLowerCase();
+			 goofyName=goofyName+bob.toLowerCase();
+		 }
 
-			
+}
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
 		
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
--- src/algorithms/GuessingPi.java
+++ src/algorithms/GuessingPi.java
@@ -8,14 +8,18 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
+	
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+String pi = "3.14";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+System.out.print(pi.charAt(0));
+System.out.print(pi.charAt(1));
+System.out.print(pi.charAt(2));
+System.out.print(pi.charAt(3));
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
@@ -33,7 +37,7 @@ public class GuessingPi {
 
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
-
+}
 }
 
 
